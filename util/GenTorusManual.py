d717c48ca75bd21e0c20c54fd464be19f5de0690
Allanah Kenny <allanahkenny@gmail.com>
2016-04-01 10:48:42 +1300

    Simplify script to manually generate torus

--- util/GenTorusManual.py
+++ util/GenTorusManual.py
@@ -1,56 +1,48 @@
+'''
+Generate a torus with manually specified R, r and mesh sizes
+'''
+
+
 import sys
 import vtk
 import numpy as np
 from configobj import ConfigObj
 
-def GenTorusManual(programArguments):
-    ''' programArguments: ini file containing model parameters'''
+# Specify the torus parameters
+r = 5/(2*np.pi); # Minor radius
+R = 6/(2*np.pi); # Major radius
+thetaResolution = 200; # Mesh in theta direction
+phiResolution = thetaResolution*(R/r); # Mesh in phi direction
 
-    # Load relevant parameters from ini file
-    conf = ConfigObj(programArguments)
-    parameters = conf['Parameters']
-    thetaMesh = parameters['thetaMesh']
+torusSource = vtk.vtkSuperquadricSource()
+torusSource.SetCenter(0.0, 0.0, 0.0)
+torusSource.SetScale(1.0, 1.0, 1.0)
+torusSource.SetToroidal(1)
+torusSource.SetThetaRoundness(1)
+torusSource.SetPhiRoundness(1)
+torusSource.SetPhiResolution(thetaResolution)   # SuperquadricSource reverses phi and theta to be confusing - this is not an error
 
-    thetaResolution = int(thetaMesh)
-    r = 5/(2*np.pi);
-    R = 6/(2*np.pi);
+torusSource.SetSize(R+r)
+torusSource.SetThickness(r)
+torusSource.SetThetaResolution(phiResolution)
 
-    torusSource = vtk.vtkSuperquadricSource()
-    torusSource.SetCenter(0.0, 0.0, 0.0)
-    torusSource.SetScale(1.0, 1.0, 1.0)
-    torusSource.SetToroidal(1)
-    torusSource.SetThetaRoundness (1)
-    torusSource.SetPhiRoundness (1)
-    torusSource.SetPhiResolution(thetaResolution)   # SuperquadricSource reverses phi and theta to be confusing - this is not an error
+tri = vtk.vtkTriangleFilter()
+tri.SetInputConnection(torusSource.GetOutputPort())
 
-    torusSource.SetSize(R+r)
-    torusSource.SetThickness (r)
-    torusSource.SetThetaResolution(thetaResolution*(R/r))
-    #torusSource.SetThetaResolution(thetaResolution)
+# The quadric has nasty discontinuities from the way the edges are generated
+# so let's pass it though a CleanPolyDataFilter and merge any points which
+# are coincident, or very close
+cleaner = vtk.vtkCleanPolyData()
+cleaner.SetInputConnection(tri.GetOutputPort())
+cleaner.SetTolerance(0.00005)
 
-    tri = vtk.vtkTriangleFilter()
-    tri.SetInputConnection(torusSource.GetOutputPort())
+cleaner.Update()
 
-    # The quadric has nasty discontinuities from the way the edges are generated
-    # so let's pass it though a CleanPolyDataFilter and merge any points which
-    # are coincident, or very close
-    cleaner = vtk.vtkCleanPolyData()
-    cleaner.SetInputConnection(tri.GetOutputPort())
-    cleaner.SetTolerance(0.00005)
+outputFileName = "torus_manual.vtp"
+print "Saving output to file", outputFileName
 
-    cleaner.Update()
+writer = vtk.vtkXMLPolyDataWriter()
+writer.SetInput(cleaner.GetOutput())
+writer.SetFileName(outputFileName)
+writer.Update()
 
-    #outputFileName = "torus_R" + majorCirc + "_tmesh" + thetaMesh + "_pmesh" + thetaMesh + ".vtp"
-    outputFileName = "torus_R6_r5_mesh" + thetaMesh + ".vtp"
-    print "Saving output to file", outputFileName
-
-    writer = vtk.vtkXMLPolyDataWriter()
-    writer.SetInput(cleaner.GetOutput())
-    writer.SetFileName(outputFileName)
-    writer.Update()
-
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print "Usage: " + sys.argv[0] + " <Program Arguments>"
-    else:
-        GenTorusManual(sys.argv[1])
